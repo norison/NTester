ef0ba7657da6612ce00013cf90e3efd9d51b7b36
Ihor Matiev <ihor.matiev@derivco.com.ua>
2022-09-17 19:18:11 +0300

    Added assertions for the create test command handler.

--- src/NTester.WebApi/Tests/NTester.Domain.Tests/Features/Tests/CreateTestCommandHandlerTests.cs
+++ src/NTester.WebApi/Tests/NTester.Domain.Tests/Features/Tests/CreateTestCommandHandlerTests.cs
@@ -38,11 +38,33 @@ public class CreateTestCommandHandlerTests
 
         // Act
         var result = await _handler.Handle(createTestCommand, CancellationToken.None);
-        
+
         // Assert
         await _dbContext.Tests.Received().AddAsync(capturedTest, CancellationToken.None);
         await _dbContext.Received().SaveChangesAsync();
 
         result.TestId.Should().Be(capturedTest.Id);
+
+        capturedTest.Title.Should().Be(createTestCommand.Title);
+        capturedTest.Description.Should().Be(createTestCommand.Description);
+        capturedTest.UserId.Should().Be(createTestCommand.UserId);
+        capturedTest.Questions.Should().NotBeEmpty();
+
+        foreach (var question in createTestCommand.Questions)
+        {
+            var testQuestion = capturedTest.Questions
+                .Should()
+                .ContainSingle(x => x.Content == question.Content && x.TestId == capturedTest.Id)
+                .Which;
+
+            foreach (var answer in question.Answers)
+            {
+                testQuestion.Answers
+                    .Should()
+                    .ContainSingle(x =>
+                        x.Content == answer.Content && x.IsCorrect == answer.IsCorrect &&
+                        x.QuestionId == testQuestion.Id);
+            }
+        }
     }
 }
